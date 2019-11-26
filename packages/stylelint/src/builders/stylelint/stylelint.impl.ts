5086f14f7d7f623f7dd65bef011bf91588adb4c8
Sebastian Guevara <rlxsebas@gmail.com>
2019-11-25 20:35:03 -0500

    style(repo): format code

--- packages/stylelint/src/builders/stylelint/stylelint.impl.ts
+++ packages/stylelint/src/builders/stylelint/stylelint.impl.ts
@@ -15,20 +15,21 @@ export function createStylelintRunner(
   { logger, workspaceRoot }: BuilderContext
 ): Observable<BuilderOutput> {
   logger.info('Start stylelint builder' + JSON.stringify(options));
-  const config = new StylelintConfiguration(logger, workspaceRoot).validateConfig(
-    options
-  );
+  const config = new StylelintConfiguration(
+    logger,
+    workspaceRoot
+  ).validateConfig(options);
   return from(lint(config)).pipe(
-    map((data: { output: any, errored: any, results: any })=> { 
+    map((data: { output: any; errored: any; results: any }) => {
       logger.warn('Errored', data.errored);
     }),
-    map(() => ({success: true })),
+    map(() => ({ success: true })),
     tap(() => logger.warn('Stylelint ran successfully')),
     catchError(e => {
-        logger.warn('Failed to ran stylelint')
-        return of({ success: false });
-      })
-  )
+      logger.warn('Failed to ran stylelint');
+      return of({ success: false });
+    })
+  );
 }
 
 export default createBuilder<json.JsonObject & StylelintBuilderSchema>(
--- packages/stylelint/src/schematics/init/init.ts
+++ packages/stylelint/src/schematics/init/init.ts
@@ -1,78 +1,76 @@
 import {
-    chain,
-    noop,
-    Rule,
-    Tree,
-    SchematicContext
-  } from '@angular-devkit/schematics';
-  import {
-    addDepsToPackageJson,
-    readJsonInTree,
-    updateJsonInTree
-  } from '@nrwl/workspace';
+  chain,
+  noop,
+  Rule,
+  Tree,
+  SchematicContext
+} from '@angular-devkit/schematics';
+import {
+  addDepsToPackageJson,
+  readJsonInTree,
+  updateJsonInTree
+} from '@nrwl/workspace';
 
-  import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { stylelintVersion, pluginsVersion } from '../../../utils/versions';
-  
-  function checkDependenciesInstalled(): Rule {
-    return (host: Tree, context: SchematicContext): Rule => {
-      const packageJson = readJsonInTree(host, 'package.json');
-      const dependencyList: { name: string; version: string }[] = [];
-      if (!packageJson.devDependencies['stylelint']) {
-        context.addTask(new NodePackageInstallTask());
-        dependencyList.push(
-          { name: 'stylelint', version: stylelintVersion },
-        );
-      }
 
-      if (!packageJson.devDependencies['@angular-plugins/stylelint']) {
-        context.addTask(new NodePackageInstallTask());
-        dependencyList.push(
-          { name: '@angular-plugins/stylelint', version: pluginsVersion },
-        );
-      }
-  
-      if (!dependencyList.length) {
-        return noop();
-      }
-  
-      return addDepsToPackageJson(
-        {},
-        dependencyList.reduce((dictionary, value) => {
-          dictionary[value.name] = value.version;
-          return dictionary;
-        }, {})
-      );
-    };
-  }
-  
-  function installDependencies(
-    dependencyList: { name: string; version: string }[]
-  ) {
-    return (host: Tree, context: SchematicContext): Rule => {
-      return addDepsToPackageJson(
-        {},
-        dependencyList.reduce((dictionary, value) => {
-          dictionary[value.name] = value.version;
-          return dictionary;
-        }, {})
-      );
-      context.logger.info(
-        '@angular-plugins/stylelint dependencies installed successfully'
-      );
-    };
-  }
-  
-  function removeDependency() {
-    return updateJsonInTree('package.json', json => {
-      json.dependencies = json.dependencies || {};
-      delete json.dependencies['stylelint'];
-      delete json.dependencies['@angular-plugins/stylelint'];
-      return json;
-    });
-  }
-  
-  export default function() {
-    return chain([removeDependency(), checkDependenciesInstalled()]);
-  }
-  
+function checkDependenciesInstalled(): Rule {
+  return (host: Tree, context: SchematicContext): Rule => {
+    const packageJson = readJsonInTree(host, 'package.json');
+    const dependencyList: { name: string; version: string }[] = [];
+    if (!packageJson.devDependencies['stylelint']) {
+      context.addTask(new NodePackageInstallTask());
+      dependencyList.push({ name: 'stylelint', version: stylelintVersion });
+    }
+
+    if (!packageJson.devDependencies['@angular-plugins/stylelint']) {
+      context.addTask(new NodePackageInstallTask());
+      dependencyList.push({
+        name: '@angular-plugins/stylelint',
+        version: pluginsVersion
+      });
+    }
+
+    if (!dependencyList.length) {
+      return noop();
+    }
+
+    return addDepsToPackageJson(
+      {},
+      dependencyList.reduce((dictionary, value) => {
+        dictionary[value.name] = value.version;
+        return dictionary;
+      }, {})
+    );
+  };
+}
+
+function installDependencies(
+  dependencyList: { name: string; version: string }[]
+) {
+  return (host: Tree, context: SchematicContext): Rule => {
+    return addDepsToPackageJson(
+      {},
+      dependencyList.reduce((dictionary, value) => {
+        dictionary[value.name] = value.version;
+        return dictionary;
+      }, {})
+    );
+    context.logger.info(
+      '@angular-plugins/stylelint dependencies installed successfully'
+    );
+  };
+}
+
+function removeDependency() {
+  return updateJsonInTree('package.json', json => {
+    json.dependencies = json.dependencies || {};
+    delete json.dependencies['stylelint'];
+    delete json.dependencies['@angular-plugins/stylelint'];
+    return json;
+  });
+}
+
+export default function() {
+  return chain([removeDependency(), checkDependenciesInstalled()]);
+}
