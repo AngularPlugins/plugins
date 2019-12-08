1d671dc646d809dbe123e9c5210c30a2509b0ef9
Sebastian Guevara <rlxsebas@gmail.com>
2019-12-08 08:50:24 -0500

    feat(npm): remove npm-login dependency

--- packages/npm/src/builders/deploy/deploy.impl.ts
+++ packages/npm/src/builders/deploy/deploy.impl.ts
@@ -13,6 +13,8 @@ import {
   readJsonFile
 } from '@nrwl/workspace/src/utils/fileutils';
 import { exec } from 'child_process';
+import { promises } from 'dns';
+
 
 export function createDeployRunner(
   options: DeployBuilderSchema,
@@ -20,17 +22,22 @@ export function createDeployRunner(
 ): Observable<BuilderOutput> {
   logger.info('Start npm deploy builder' + JSON.stringify(options));
 
-  const workspacePackageJSON = readJsonFile('package.json');
 
   const libraryPackageJson = readJsonFile(options.packageJsonPath);
 
   const unlinkFileObservable = bindNodeCallback(unlink);
   const execObservable = bindNodeCallback(exec);
+
   return unlinkFileObservable(options.packageJsonPath).pipe(
-    map(() => {
+    switchMap(()=>{
+      return from(executeCommand(logger,`git describe --tags`))
+    }),
+    map((version) => {
+      logger.warn(`The version to use ${version
+        }`);
       writeJsonFile(options.packageJsonPath, {
         ...libraryPackageJson,
-        version: workspacePackageJSON.version
+        version: version.replace('\n',"").replace('v',"")
       });
     }),
     switchMap(()=>{ 
@@ -39,14 +46,30 @@ export function createDeployRunner(
     map(()=>{
         return { success: true };
     }),
-    tap(() => logger.warn('npm deploy ran successfully')),
+    tap(() => logger.info('npm deploy ran successfully')),
     catchError(e => {
-      logger.error('Failed to ran npm deploy', e);
+      logger.error(`Failed to ran npm deploy ${e}`);
       return of({ success: false });
     })
   );
 }
 
+
+function executeCommand(logger, command): Promise<any>{
+  var ChildProcess = require('child_process');
+
+  return new Promise((res,rej) => {
+    const a  = ChildProcess.exec('git describe --tags');
+    a.stdout.on('data', data => {
+        logger.info(`Current Tag Version ${data}`);
+        res(data);
+      });
+      a.stderr.on('data', err => {
+        logger.error(`There is an error ${err}`);
+        rej(err);
+      });
+  });
+}
 export default createBuilder<json.JsonObject & DeployBuilderSchema>(
   createDeployRunner
 );
